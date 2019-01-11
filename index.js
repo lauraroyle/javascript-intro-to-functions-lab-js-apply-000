6aa5fce52199e03fc527d957fc4c177e09f3ad78
Laura Royle <lauraamie.royle@gmail.com>
2019-01-11 21:53:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can\'t hear you!"
-}
-
-}
-
-function sayHiToGrandma(string) {
+}{
   if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
+}
 }
